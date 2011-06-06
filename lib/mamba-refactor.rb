6a8de121ed830b0f4ed9f4e221e7cc4542c60b03
rogwfu <roger.seagle@gmail.com>
2011-06-05 20:07:45 -0400

    Added validation of fuzzing algorithm to creation/Added Mangle inheritance as test

--- lib/mamba-refactor.rb
+++ lib/mamba-refactor.rb
@@ -1,2 +1,3 @@
 require 'mamba-refactor/fuzzer'
 require 'mamba-refactor/reporter'
+require 'mamba-refactor/algorithms/mangle'
---
+++ lib/mamba-refactor/algorithms/mangle.rb
@@ -0,0 +1,16 @@
+module Mamba
+	class Mangle < Fuzzer
+
+		def self.generate_config()
+			puts "Generating a Configuration for Mangle"
+		end
+
+		def initialize()
+			super()
+		end
+
+		def fuzz()
+
+		end
+	end
+end
