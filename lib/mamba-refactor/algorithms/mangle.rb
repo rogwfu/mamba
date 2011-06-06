94b9ee801eb39028de4713c21f5c7894f708642f
rogwfu <roger.seagle@gmail.com>
2011-06-05 21:17:29 -0400

    Mangle default configuration file generation done

--- lib/mamba-refactor/algorithms/mangle.rb
+++ lib/mamba-refactor/algorithms/mangle.rb
@@ -1,9 +1,19 @@
 module Mamba
 	class Mangle < Fuzzer
 
+        DEFAULT_HEADER_SIZE = 1024
+        DEFAULT_NUMBER_CASES = 1024
+        DEFAULT_BASE_FILE = "tests/test2"
+        DEFAULT_OFFSET = 0 
+
 		# Generates the YAML configuration file for Mangle fuzzer
 		def self.generate_config()
-			puts "Generating a Configuration for Mangle"
+                mangleConfig = Hash.new()
+                mangleConfig['Header Size'] = Mamba::Mangle::DEFAULT_HEADER_SIZE
+                mangleConfig['Number of Testcases'] = Mamba::Mangle::DEFAULT_NUMBER_CASES
+                mangleConfig['Default Offset'] = Mamba::Mangle::DEFAULT_OFFSET
+                mangleConfig['Basefile'] = Mamba::Mangle::DEFAULT_BASE_FILE
+				YAML::dump(mangleConfig)
 		end
 
 		# Initializes Mangle fuzzer and the generic fuzzer class
