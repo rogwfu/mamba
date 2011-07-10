7c8cadeef726c92a96448cb3d66718d86dc0cd0a
rogwfu <roger.seagle@gmail.com>
2011-07-10 18:52:48 -0400

    Fixed distributed mangle file name to match scheme

--- lib/mamba-refactor.rb
+++ lib/mamba-refactor.rb
@@ -12,5 +12,5 @@ require 'mamba-refactor/algorithms/simple_ga'
 #
 # Distributed Algorithms
 #
-require 'mamba-refactor/algorithms/distributed-mangle'
+require 'mamba-refactor/algorithms/distributed_mangle'
 require 'mamba-refactor/algorithms/distributed_simple_ga'
