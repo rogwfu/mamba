c58ba1f2fc518867d7d68e2998a9edda3c3308bf
rogwfu <roger.seagle@gmail.com>
2011-07-13 11:36:59 -0400

    Documented the mutation function for the byte_ga

--- lib/mamba-refactor/algorithms/byte_ga.rb
+++ lib/mamba-refactor/algorithms/byte_ga.rb
@@ -2,10 +2,10 @@ module Mamba
 	module Algorithms
 		class ByteGeneticAlgorithm < SimpleGeneticAlgorithm 
 
+			# Mutate a file by randoming selecting bytes through out the file with 
+			# the probability for each byte being mutated being the Mutation Rate
 			# @param [Array] An array of children file descriptors
 			def mutate(children)
-				@logger.info("Byte Genetic algorithm mutator")
-
 				randomMutator = RandomGenerator.new() 	
 				children.each do |child|
 					tmpFile = File.new("tests" + File::SEPARATOR + "temp-test", 'wb+')
