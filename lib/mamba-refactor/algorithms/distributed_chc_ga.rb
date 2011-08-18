3184874bf9388eabd1510961843ba8e720a613ba
rogwfu <roger.seagle@gmail.com>
2011-08-17 22:50:44 -0400

    Start of overwriting the received results distributed method

--- lib/mamba-refactor/algorithms/distributed_chc_ga.rb
+++ lib/mamba-refactor/algorithms/distributed_chc_ga.rb
@@ -2,6 +2,25 @@ module Mamba
 	module Algorithms
 		class DistributedCHCGeneticAlgorithm < DistributedSimpleGeneticAlgorithm
 			include Mamba::Algorithms::Helpers::CHC
+
+			# Function to handle CHC results and evolution
+			#
+			def results(header, payload)
+				chromosomeInfo = payload.split(".")
+				@logger.info("Chromosome[#{chromosomeInfo[1]}]: #{chromosomeInfo[2]}")
+				@reporter.numCasesRun = @reporter.numCasesRun + 1
+				
+				if(@organizer)
+					@population.push(Chromosome.new(chromosomeInfo[1].to_i(), chromosomeInfo[2]))
+					# Check for condition to stop and condition to stop and evolve
+					if(@population.size == @simpleGAConfig['Population Size']) then
+						@logger.info("Reached the full population size")
+						@topic_exchange.publish("shutdown", :key => "commands")
+					end
+
+				end
+			end
+
 		end
 	end
 end
