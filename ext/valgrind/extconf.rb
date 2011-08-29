757e58c131190d954ce7339af13654afad7edcda
rogwfu <roger.seagle@gmail.com>
2011-08-29 16:20:36 -0400

    Added the valgrind patches and extension script

---
+++ ext/valgrind/extconf.rb
@@ -0,0 +1,31 @@
+require 'mkmf'
+
+#
+# Check for required tools
+#
+tools = Hash.new()
+%w(svn patch make).each do |toolName|
+	toolLoc = find_executable(toolName)
+	if(!toolLoc) then
+		raise "Error: #{toolName} not found in #{ENV['PATH']}"
+	else
+		tools[toolName] = toolLoc
+	end
+end
+valgrindTag = "VALGRIND_3_6_1"
+
+puts "===== Pulling valgrind tag: #{valgrindTag} ====="
+system("#{tools["svn"]} co svn://svn.valgrind.org/valgrind/tags/#{valgrindTag} trunk")
+
+puts "===== Patching Valgrind ====="
+system("cp ../../valgrind-patches/*.patch trunk/")
+#system("cd trunk ; #{tools["patch"]} -p0 < callgrind.3.6.1.patch")
+system("cd trunk ; #{tools["patch"]} -p0 < rufus.3.6.0.patch")
+system("cd trunk ; #{tools["patch"]} -p0 < signals.patch")
+
+puts "===== Building Valgrind ====="
+system("cd trunk ; ./autogen.sh ; ./configure --prefix=`pwd`/inst")
+system("cd trunk ; #{tools["make"]}; #{tools["make"]} install")
+
+# Appease packaging library
+create_makefile("")
