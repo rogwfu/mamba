cfc26be8598b04fdb6d8b5881162c8246f6e6a18
rogwfu <roger.seagle@gmail.com>
2011-09-14 00:55:44 -0400

    Remvoed extraneous commented out code

--- lib/mamba-refactor/algorithms/distributed_chc_ga.rb
+++ lib/mamba-refactor/algorithms/distributed_chc_ga.rb
@@ -57,39 +57,6 @@ module Mamba
 				super(mambaConfig)
 			end
 
-			# Function to handle CHC results and evolution
-#			def results(header, payload)
-#				chromosomeInfo = payload.split(".")
-#				@logger.info("Chromosome[#{chromosomeInfo[1]}]: #{chromosomeInfo[2]}")
-#				@reporter.numCasesRun = @reporter.numCasesRun + 1
-#				
-#				# Check the role of this node
-#				if(@organizer)
-#					@population.push(Chromosome.new(chromosomeInfo[1].to_i(), BigeDecimal.new(chromosomeInfo[2])))
-#
-#					# Check to see if the current 
-#					# Check for condition to stop and condition to stop and evolve
-#					if(@population.size == @simpleGAConfig['Population Size']) then
-#						@logger.info(@population.inspect())
-#						statistics()
-#
-#						@nextGenerationNumber = @nextGenerationNumber + 1
-#						unless (@reporter.numCasesRun >= (@simpleGAConfig['Maximum Generations'] * @simpleGAconfig['Population Size'])) then
-#							@topic_exchange.publish("shutdown", :key => "commands")
-#							evolve() 
-#
-#							# Reseed the next population
-#							@testSetMappings.each_key do |key|
-#								seed_distributed(key)
-#							end
-#
-#						else
-#							@topic_exchange.publish("shutdown", :key => "commands")
-#						end
-#					end
-#				end
-#			end
-
 		end
 	end
 end
