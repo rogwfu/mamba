193500ed05d7bbf904876da790e00867ffdce153
rogwfu <roger.seagle@gmail.com>
2011-11-10 20:33:35 -0500

    Fixed the @rpath problem (need to go back and clean it up)

--- lib/mamba-refactor/tools/otool.rb
+++ lib/mamba-refactor/tools/otool.rb
@@ -6,7 +6,7 @@ module Mamba
 			# @param [String] Path of the object file to interrogate
 			# @param [Boolean] Enable 64 bit architecture support
 			# @returns nil if otool is not found
-			def initialize(objectFile)
+			def initialize(objectFile, architecture)
 				@otool = find_executable0('otool')
 				if(@otool == nil) then
 					return(nil)
@@ -26,16 +26,43 @@ module Mamba
                 else
                     @architecture = "i386"
                 end
+
+				# Stash all rpath commands for latter reference 
+				@rpathCommands = Array.new()	
 			end
 
 			# Reads the shared libraries from an objects macho header
+			# Note collapse this down to one pass for rpath command and load commands
 			def read_header()
 				# Pull an object off the array
 				object = @needed.shift()
 				@found[object] = true
 
-				# Find all shared libraries linked with the object
 				output = ""
+
+
+				# Find all rpath commands 
+				ret = IO.popen("#{@otool} -arch #{@architecture} -l \"#{object}\"", 'r') do |f|
+					output = f.read()
+				end
+
+				loadCommands = output.split("\n")
+				loadCommands.each_index do |idx|
+					if(loadCommands[idx].include?("cmd LC_RPATH")) then
+						rpath = loadCommands[idx+2]
+						rpath.gsub!(/^\s+path\s*/, "")
+						rpath.gsub!(/\s+\(.*?\)\s*/, "")
+
+						# Quick hack to fix executable path in load path
+						if(rpath.start_with?("@executable_path")) then
+							rpath.gsub!(/@executable_path/, @executablePath)
+						end
+
+						@rpathCommands << rpath
+					end
+				end
+
+				# Find all shared libraries linked with the object 
 				ret = IO.popen("#{@otool} -arch #{@architecture} -L \"#{object}\"", 'r') do |f|
 					output = f.read()
 				end
@@ -48,15 +75,39 @@ module Mamba
 				linkedObjects.each do |sharedObject|
 					sharedObject.sub!(/\(.*\)/, '').strip!().chomp!()
 
-					# Fix executable path variable in linker
+					# Fix executable path variable in load command
 					if(sharedObject.start_with?("@executable_path")) then
 						sharedObject.gsub!(/@executable_path/, @executablePath)
+					end
+
+					# Fix rpath variable in load command
+					if(sharedObject.start_with?("@rpath")) then
+						sharedObject = fixup_rpath(sharedObject)
 					end
 
 					if(!@found.has_key?(sharedObject)) then
 						@needed.push(sharedObject)
 					end
 				end
+			end
+
+			# Fixup an load command using rpath with the full path 
+			# @param [String] Object from the load commands
+			# @returns [String] Fully quantified path for the shared object
+			def fixup_rpath(sharedObject)
+				# Remove the rpath
+				sharedObject.gsub!(/@rpath/, "")
+			
+				# Try different rpaths
+				@rpathCommands.each do |rpath|
+					objectFullPath = rpath + sharedObject
+					if(File.exists?(objectFullPath)) then		
+						return(objectFullPath)	
+					end
+				end
+
+				# Error case (couldn't find it so return with the @rpath)
+				return("@rpath" + sharedObject)
 			end
 
 			# Start recursively interrogating object files with otool
