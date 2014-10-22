f9c6fcaff910e05a89cf257f282bad396559fbac
Roger Seagle <roger.seagle@gmail.com>
2014-10-22 15:23:58 -0700

    Initial start for Linux compatibility, changed install paths to ~/.mamba

--- ext/lldb/extconf.rb
+++ ext/lldb/extconf.rb
@@ -15,12 +15,18 @@ end
 # Configure the correct url
 case RbConfig::CONFIG["host_os"]
 when /^darwin(10|11|12|13|14)\.\d+(\.\d+)?$/
+when /^linux-gnu$/
 else
 	raise "Error: Unsupported Operating System (#{RbConfig::CONFIG["host_os"]})"
 end
 
 # Copy the files to the local path (need to make this more dynamic
-installDir = "/usr/local/bin"
+installDir = ENV['HOME'] +  File::SEPARATOR + ".mamba"
+unless File.directory?(installDir)
+  FileUtils.mkdir_p(installDir)
+end
+
+# Copy useful scripts
 FileUtils.cp(File.dirname(__FILE__) + "/lldb-func-tracer.py", installDir, :verbose => true)
 FileUtils.cp(File.dirname(__FILE__) + "/lldbutil.py", installDir, :verbose => true)
 
