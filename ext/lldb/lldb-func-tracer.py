23f4ee88e240390667129741196dc4f22bb51da9
Roger Seagle <roger.seagle@gmail.com>
2014-11-03 17:30:03 +0000

    Fixed the lldb function tracer

--- ext/lldb/lldb-func-tracer.py
+++ ext/lldb/lldb-func-tracer.py
@@ -4,7 +4,7 @@
 # [  0] B5B5215F-38C0-364D-BDA4-35D674FFCEC4 0x00007fff81cf0000 /System/Library/PrivateFrameworks/CorePDF.framework/Versions/A/CorePDF 
 
 import sys
-sys.path.append("/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Resources/Python")
+#sys.path.append("/Applications/Xcode.app/Contents/SharedFrameworks/LLDB.framework/Resources/Python")
 import lldb
 import lldbutil 
 import os
@@ -34,16 +34,15 @@ def printBreakStats(SIG, FRM):
             blHitCount = breakPointLocationInfo[3].split("=")[1]
             blHitCount = int(blHitCount)
             if(blHitCount > 0):
-              hit = ET.SubElement(root, 'hit')
-              funcname = ET.SubElement(hit, "funcname")
-              funcname.text = blFuncName
-              offset = ET.SubElement(hit, "offset")
-              offset.text = blLoadAddress
-              count = ET.SubElement(hit, "count")
-              count.text = str(blHitCount) 
+                hit = ET.SubElement(root, 'hit')
+                funcname = ET.SubElement(hit, "funcname")
+                funcname.text = blFuncName
+                offset = ET.SubElement(hit, "offset")
+                offset.text = blLoadAddress
+                count = ET.SubElement(hit, "count")
+                count.text = str(blHitCount) 
     traceFile = open(xmlTrace, "w")
     ET.ElementTree(root).write(traceFile, pretty_print=True, xml_declaration=True) 
-    exit(1)
 
 
 # Catch SIGTERM signal to initiate printing stats 
@@ -53,7 +52,7 @@ except RuntimeError,m:
     print "Got an execption setting the SIGSTOP handler"
 
 # Run commands using the lldb command line
-def run_commands(command_interpreter, commands):
+def run_commands(command_interpreter, commands, options):
     return_obj = lldb.SBCommandReturnObject()
     for command in commands:
         command_interpreter.HandleCommand( command, return_obj )
@@ -69,7 +68,7 @@ def main(argv):
     epilog='''Examples:
         % ./lldb-test.py -x lldbtrace.xml -s sharedlibrary -- /Applications/Preview.app/Contents/MacOS/Preview
             '''
-    parser = optparse.OptionParser(description=description, prog='lldb-test',usage='usage: lldb-test [options] program [arg1 arg2]', epilog=epilog)
+    parser = optparse.OptionParser(description=description, prog='lldb-func-tracer.py',usage='usage: lldb-func-tracer.py [options] -- program [arg1 arg2]', epilog=epilog)
     optparse.OptionParser.format_epilog = lambda self, formatter: self.epilog
     parser.add_option('-e', '--environment', action='append', type='string', metavar='ENV', dest='env_vars', help='Environment variables to set in the inferior process when launching a process.')
     parser.add_option('-s', '--shlib', type='string', dest='shlibs', metavar='SHLIB', help='Specify the shared library to trace functions')
@@ -82,67 +81,53 @@ def main(argv):
 
     # Set the executable
     exe = args.pop(0)
-    
+
     # Create a new debugger instance
     debugger = lldb.SBDebugger.Create()
     command_interpreter = debugger.GetCommandInterpreter()
+
+    launch_info = None
+    launch_info = lldb.SBLaunchInfo(args)
+    if options.env_vars:
+        launch_info.SetEnvironmentEntries(options.env_vars, True)
 
     # Setup a target to debug
     global target
     global xmlTrace
     xmlTrace = options.xmlfile
-#    print xmlTrace
-#    print exe
-    #target = debugger.CreateTargetWithFileAndArch(exe, lldb.LLDB_ARCH_DEFAULT)
-    ret = run_commands(command_interpreter, ['target create ' + exe])
-    launch_info = None
-    launch_info = lldb.SBLaunchInfo(args)
-    if options.env_vars:
-      print options.env_vars
-      launch_info.SetEnvironmentEntries(options.env_vars, True)
-
-    # Grab the target from the debugger
-    target = debugger.GetTargetAtIndex(0)
+    target = debugger.CreateTargetWithFileAndArch(exe, lldb.LLDB_ARCH_DEFAULT)
     if target:
         error = lldb.SBError()
-        # Set all the breakpoints 
-        ret = run_commands(command_interpreter, ['breakpoint set --func-regex=. --shlib=' + options.shlibs])
-        # Launch the process. Since we specified synchronous mode, we won't return
-        # from this function until we hit the breakpoint at main
+        lib_breakpoints = target.BreakpointCreateByRegex(".", options.shlibs)	
 
-        # Set the running arguments
-        ret = run_commands(command_interpreter, ['settings set target.run-args ' + ' '.join(args)])
-
-        # Run the executable
-        ret = run_commands(command_interpreter, ['run'])
-        #process = target.Launch(launch_info, error) 
-        process = target.GetProcess()
+        process = target.Launch(launch_info, error) 
         if process and process.GetProcessID() != lldb.LLDB_INVALID_PROCESS_ID:
+            state = process.GetState ()
             pid = process.GetProcessID()
             listener = lldb.SBListener("event_listener")
+
             # sign up for process state change events
             process.GetBroadcaster().AddListener(listener, lldb.SBProcess.eBroadcastBitStateChanged)
+            stop_idx = 0
             done = False
             while not done:
                 event = lldb.SBEvent()
                 if listener.WaitForEvent(options.event_timeout, event):
                         state = lldb.SBProcess.GetStateFromEvent (event)
                         if state == lldb.eStateStopped:
-                            #print "State is stopped"
                             thread = lldbutil.get_stopped_thread(process, lldb.eStopReasonBreakpoint)
                             if thread == None:
-#                                print "Error: No Stopped Thread"
+                                print "Error: No Stopped Thread"
+                                process.Continue()
                                 exit(1)
                             else:
-#                                print "Breakpoint"
                                 process.Continue()
                         elif state == lldb.eStopReasonSignal:
-                            #print "Got a signal"
                             next
                         elif state == lldb.eStateRunning:
-                            #print "Running"
                             next
                         elif state == lldb.eStateExited:
+                            done = True
                             exit_desc = process.GetExitDescription()
                             printBreakStats(None, None)
                             if exit_desc:
@@ -152,7 +137,6 @@ def main(argv):
                         elif state == lldb.eStateCrashed:
                             process.Continue()
                         elif state == lldb.eStateUnloaded:
-#                            print "process %u unloaded, this shouldn't happen" % (pid)
                             done = True
                         elif state == lldb.eStateConnected:
                             print "process connected"
@@ -171,3 +155,16 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+# Got To know functions
+
+#triple = "x86_64-apple-macosx"
+#module = target.AddModule ("/usr/lib/system/libsystem_c.dylib", triple, None, "/build/server/a/libsystem_c.dylib.dSYM")
+#target.SetSectionLoadAddress(module.FindSection("__TEXT"), 0x7fff83f32000)
+#                                for sec in target.module[options.shlibs].section_iter():
+#                                        if sec.GetName() == ".text" or sec.GetName() == "__TEXT":
+#                                                print sec
+#                                                print sec.GetLoadAddress(target)
+#                                exit(0)
+                                #print "Breakpoint"
+
