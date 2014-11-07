ebcf6de848ca0b6f8a34c68850f38a553926b692
Roger Seagle <roger.seagle@gmail.com>
2014-11-07 14:00:21 +0000

    Added back local rabbitmq install, due to root restriction

---
+++ ext/rabbitmq/extconf.rb
@@ -0,0 +1,46 @@
+require 'mkmf'
+
+#
+# Check for required tools
+#
+tools = Hash.new()
+%w(curl tar erl).each do |toolName|
+	toolLoc = find_executable(toolName)
+	if(!toolLoc) then
+		raise "Error: #{toolName} not found in #{ENV['PATH']}"
+	else
+		tools[toolName] = toolLoc
+	end
+end
+
+# Configure the correct url
+# Using 2.0.0 instead of latest due to default exchange usage, for more info:
+# http://groups.google.com/group/rabbitmq-discuss/browse_thread/thread/eac656ba68758021?fwc=1
+# Use erlang: otp_src_R14B
+rabbitmqURL = ""
+case RbConfig::CONFIG["host_os"]
+when /^darwin(10|11|12)\.\d+(\.\d+)?$/
+when /^linux-gnu$/
+#	rabbitmqURL = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.3.1/rabbitmq-server-2.3.1.tar.gz"
+#	rabbitmqURL = "http://www.rabbitmq.com/releases/rabbitmq-server/v1.7.2/rabbitmq-server-1.7.2.tar.gz"
+#	rabbitmqURL = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.0.0/rabbitmq-server-2.0.0.tar.gz"
+	rabbitmqURL = "https://www.rabbitmq.com/releases/rabbitmq-server/v3.4.1/rabbitmq-server-3.4.1.tar.gz"
+else
+	raise "Error: Unsupported Operating System (#{RbConfig::CONFIG["host_os"]})"
+end
+
+# Copy the files to the local path (need to make this more dynamic
+installDir = ENV['HOME'] +  File::SEPARATOR + ".mamba"
+unless File.directory?(installDir)
+  FileUtils.mkdir_p(installDir)
+end
+
+# Download, Extract, and Make rabbitmq (if needed)
+if(!find_executable("rabbitmq-server")) then
+	system("#{tools["curl"]} -O #{rabbitmqURL}") 
+	system("#{tools["tar"]} -C #{ENV['USER']}/.mamba xvzf #{rabbitmqURL.split('/')[-1]}")
+	system("cd #{rabbitmqURL.split('/')[-1].gsub(/\.tar\.gz$/, "")} ; make ")
+end
+
+# Appease packaging library
+create_makefile("")
